dad9a2b80b46431a715b5891283684edbc56e19e
NavrasK <trailmixerstudios@gmail.com>
2018-05-12 17:35:49 -0600

    cleaning packages

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-
-const options = {
-    uri: 'https://www.bagoum.com/db/A4V',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-};
-
-rp(options)
-    .then(($) => {
-        console.log($);
-        $('#DB-cardListCont').each(function(i, elem) {
-            console.log($(this).text());
-        });
-    })
-    .catch((err) => {console.log(err);})
+var loc = "A4V";
+$.get('https://www.bagoum.com/' + loc, function(response) {
+  console.log(response);
+});
