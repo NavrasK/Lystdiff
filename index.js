ca8ff36b90e280b1cfda21b50eb4562fdb589d42
NavrasK <trailmixerstudios@gmail.com>
2018-05-12 17:14:10 -0600

    scraping testing

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+
+const options = {
+    uri: 'https://www.bagoum.com/db/A4V',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
+
+rp(options)
+    .then(($) => {
+        console.log($);
+        $('#DB-cardListCont').each(function(i, elem) {
+            console.log($(this).text());
+        });
+    })
+    .catch((err) => {console.log(err);})
